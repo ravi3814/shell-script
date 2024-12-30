d6882a9dc0c548113dbdb1524617b7dd927d89ac
Ravi-Gandham <122512741+Ravi-Gandham@users.noreply.github.com>
2024-12-30 16:21:48 +0530

    Variables

---
+++ 03-Variables-assign.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# the line is commented
+PERSON1=RAMESH
+PERSON2=SURESH
+echo "$PERSON1::Hi $PERSON2"
+echo "$PERSON2::Hello $PERSON1"
+echo "$PERSON1::How are you doing"
+echo "$PERSON2::I am Good how are you"
