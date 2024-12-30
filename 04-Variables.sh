b358c37a4f90f0365fd19e9fb6e8091c85c1a0c2
Ravi-Gandham <122512741+Ravi-Gandham@users.noreply.github.com>
2024-12-30 16:29:42 +0530

    conversation added for 4 files

---
+++ 04-Variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# the line is commented
+PERSON1=RA$1
+PERSON2=SUR$2
+echo "$PERSON1::Hi $PERSON2"
+echo "$PERSON2::Hello $PERSON1"
+echo "$PERSON1::How are you doing"
+echo "$PERSON2::I am Good how are you"
