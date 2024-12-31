0ea04ba4c6694c21fdc51fdd500836c613666ac5
Ravi-Gandham <122512741+Ravi-Gandham@users.noreply.github.com>
2024-12-31 15:44:30 +0530

    shell scipt on data tyoes

--- 07-Array.sh
+++ 07-Array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-MOVIES=("Pushpa", "RRR" , "DEVARAA")
+MOVIES=("Pushpa" "RRR" "DEVARAA")
 #index starts from 0 , size is 3
 
 echo "First Movie : ${MOVIES[0]}"
-echo "Second Movie :${MOVIES[1]}"
+echo "Second Movie: ${MOVIES[1]}"
 echo "Third Movie : ${MOVIES[2]}"
 
 echo "All Movies are : ${MOVIES[@]}"
