bd6953af14c28b76b47967b52c7b84d86fc69e6f
Ravi-Gandham <122512741+Ravi-Gandham@users.noreply.github.com>
2025-01-02 13:06:03 +0530

    shell scipt on data tyoes

--- 11-functions.sh
+++ 11-functions.sh
@@ -11,3 +11,30 @@ VALIDATE(){
         echo "$2 ... SUCCESS"
     fi
 }
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo "MySQL is already ... INSTALLED"
+fi
+
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo "Git is already ... INSTALLED"
+fi
