cdf7476e80a0a4360bc12532e2b852c833be0af2
Ravi-Gandham <122512741+Ravi-Gandham@users.noreply.github.com>
2025-01-23 12:00:40 +0530

    expense shell

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -17,4 +17,4 @@ done <<< $DISK_USAGE
 
 echo -e "Message: $MSG"
 
-echo "$MSG" | mutt -s "High Disk Usage" gravi3814.devops@gmail.com
+ echo "$MSG" | mutt -s "High Disk Usage" gravi3814.devops@gmail.com
